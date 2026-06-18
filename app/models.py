96adabd294438f89a96cabb766548ec9281c9558
clinicaveterinaria-arch <clinicaveterinaria@losaromos1.com>
2026-06-18 07:52:31 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -169,10 +169,7 @@ class Sale(Base):
         Float,
         default=0
     )
-    payment_method: Mapped[str] = mapped_column(
-        String(50),
-        default='Efectivo'
-    )
+ 
     patient_id: Mapped[Optional[int]] = mapped_column(
         ForeignKey('patients.id'),
         nullable=True,
