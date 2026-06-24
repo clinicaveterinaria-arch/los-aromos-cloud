b2a159e60baf8115a18668a664dc61ff7cb5aa42
clinicaveterinaria-arch <clinicaveterinaria@losaromos1.com>
2026-06-24 10:35:27 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -460,10 +460,10 @@ def agenda(
         .order_by(Appointment.start_time)
         .all()
     )
-        appointments_count_by_day = {}
-        for appointment in appointments:
-            day_key = appointment.appointment_date.day
-            appointments_count_by_day[day_key] = appointments_count_by_day.get(day_key, 0) + 1
+    appointments_count_by_day = {}
+    for appointment in appointments:
+        day_key = appointment.appointment_date.day
+        appointments_count_by_day[day_key] = appointments_count_by_day.get(day_key, 0) + 1
     owners = db.query(Owner).order_by(Owner.name).limit(300).all()
     patients = db.query(Patient).order_by(Patient.name).limit(300).all()
 
